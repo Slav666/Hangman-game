3304745fc49b72d3970f90d62714fb896523f808
Slawomir Dyk <slawomirdyk@gmail.com>
2019-04-20 17:00:23 +0100

    Password invisible by player

--- hangman.js
+++ hangman.js
@@ -1,8 +1,18 @@
 var passwordToGuess = "Actions speak louder than words.";
 passwordToGuess = passwordToGuess.toUpperCase();
 
+var passwordLength = passwordToGuess.length;
+var password1 = "";
+for (i = 0; i < passwordLength; i++) {
+    if (passwordToGuess[i] == " ") {
+        password1 = password1 + " "
+    } else {
+                password1 = password1 + "-"
+            }
+    
+}
 function writePasswordToGuess(){
-    document.getElementById("game-board").innerHTML = passwordToGuess;
+    document.getElementById("game-board").innerHTML = password1;
 }
 
-window.onload = writePasswordToGuess;
+window.onload = writePasswordToGuess; 
