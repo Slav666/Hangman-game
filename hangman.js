b7eb414ed5d90795d66b47d6962bdfce0618936d
Slawomir Dyk <slawomirdyk@gmail.com>
2019-04-20 22:27:15 +0100

    In hangman.js function start added, in css letters added

--- hangman.js
+++ hangman.js
@@ -4,15 +4,30 @@ passwordToGuess = passwordToGuess.toUpperCase();
 var passwordLength = passwordToGuess.length;
 var password1 = "";
 for (i = 0; i < passwordLength; i++) {
-    if (passwordToGuess[i] == " ") {
+    if (passwordToGuess.charAt(i) == " ") {
         password1 = password1 + " "
     } else {
                 password1 = password1 + "-"
-            }
+    }
     
 }
+
 function writePasswordToGuess(){
     document.getElementById("game-board").innerHTML = password1;
 }
 
-window.onload = writePasswordToGuess; 
+window.onload = start; 
+
+
+function start(){
+
+    var div_content = " ";
+
+    for (i = 0; i < 26; i++){
+        div_content = div_content + '<div class="letter">A</div>';
+        if((i+ 1) % 7 == 0) div_content = div_content + '<div style="clear:both;"></div>';
+    }
+    document.getElementById("alphabet").innerHTML = div_content;
+
+    writePasswordToGuess();
+}
